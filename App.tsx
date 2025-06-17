cbfad6eec20fd94dbe84fc7f3de9f8cf21e2ded2
DouglasFeliphe <douglas.feliphe@going2.com.br>
2025-06-17 09:51:44 -0300

    fix: status bar layout

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components/native';
 import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message';
 import HomeScreen from '@/screens/HomeScreen';
@@ -9,15 +9,20 @@ import FavoritesScreen from '@/screens/FavoritesScreen';
 import { myTheme } from '@/theme/theme';
 import React from 'react';
 import { toastConfig } from '@/theme/toastConfig';
+import { StatusBar } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <React.Fragment>
-      <NavigationContainer>
-        <ThemeProvider theme={myTheme}>
-          <SafeAreaProvider>
+    <SafeAreaProvider>
+      <StatusBar
+        backgroundColor={myTheme.colors.white}
+        barStyle="dark-content"
+      />
+      <SafeAreaView style={{ flex: 1 }} edges={['right', 'left']}>
+        <NavigationContainer>
+          <ThemeProvider theme={myTheme}>
             <Stack.Navigator
               screenOptions={{
                 headerShown: false,
@@ -28,10 +33,10 @@ export default function App() {
               <Stack.Screen name="NewsDetail" component={NewsDetailScreen} />
               <Stack.Screen name="Favorites" component={FavoritesScreen} />
             </Stack.Navigator>
-          </SafeAreaProvider>
-        </ThemeProvider>
-      </NavigationContainer>
-      <Toast config={toastConfig} />
-    </React.Fragment>
+          </ThemeProvider>
+        </NavigationContainer>
+        <Toast config={toastConfig} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
